fa331b3ffd8122c871eee0b0c00b75acfd6859ac
orihpt <ori9070.hpt@gmail.com>
2020-08-11 14:30:44 +0300

    Initial Commit

---
+++ passDataSimply/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passDataSimply
+//
+//  Created by אורי האופטמן on 11/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
